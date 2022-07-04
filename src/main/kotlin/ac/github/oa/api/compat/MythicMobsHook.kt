6ee0870d6b49a7e1513c4a9d43b76296e4c9f326
KunSs <wen20020419>
2022-07-04 21:56:19 +0800

    fix

--- src/main/kotlin/ac/github/oa/api/compat/MythicMobsHook.kt
+++ src/main/kotlin/ac/github/oa/api/compat/MythicMobsHook.kt
@@ -17,40 +17,38 @@ object MythicMobsHook {
     @SubscribeEvent(bind = "io.lumine.xikage.mythicmobs.api.bukkit.events.MythicMobDeathEvent")
     fun e(ope: OptionalEvent) {
         val event = ope.get<MythicMobDeathEvent>()
-        if (event.killer is Player) {
-            val mm = event.mobType
-            val drops = event.drops
-            val dropLockKiller = mm.config.getBoolean("OriginOptions.DropLockKiller", false)
+        val mm = event.mobType
+        val drops = event.drops
+        val dropLockKiller = mm.config.getBoolean("OriginOptions.DropLockKiller", false)
 
-            for (str in mm.config.getStringList("OriginOptions.Drops")) {
-                if (str.contains(" ")) {
-                    val args = str.split(" ")
-                    var amount = 1
-                    if (args.size > 2 && args[2].isNotEmpty() && random()
-                            .nextDouble() > args[2].replace("[^0-9.]".toRegex(), "").toDouble()
-                    ) {
-                        continue
-                    }
-                    if (args.size > 1 && args[1].isNotEmpty()) { // 数量判断
-                        if (args[1].contains("-") && args[1].split("-".toRegex()).toTypedArray().size > 1) {
-                            val i1 =
-                                args[1].split("-".toRegex()).toTypedArray()[0].replace("[^\\d]".toRegex(), "").toInt()
-                            val i2 =
-                                args[1].split("-".toRegex()).toTypedArray()[1].replace("[^\\d]".toRegex(), "").toInt()
-                            if (i1 > i2) {
-                                info("MythicMobs - Drop Random Error: " + mm.displayName + " - " + str)
-                            } else {
-                                amount = random().nextInt(i2 - i1 + 1) + i1
-                            }
+        for (str in mm.config.getStringList("OriginOptions.Drops")) {
+            if (str.contains(" ")) {
+                val args = str.split(" ")
+                var amount = 1
+                if (args.size > 2 && args[2].isNotEmpty() && random()
+                        .nextDouble() > args[2].replace("[^0-9.]".toRegex(), "").toDouble()
+                ) {
+                    continue
+                }
+                if (args.size > 1 && args[1].isNotEmpty()) { // 数量判断
+                    if (args[1].contains("-") && args[1].split("-".toRegex()).toTypedArray().size > 1) {
+                        val i1 =
+                            args[1].split("-".toRegex()).toTypedArray()[0].replace("[^\\d]".toRegex(), "").toInt()
+                        val i2 =
+                            args[1].split("-".toRegex()).toTypedArray()[1].replace("[^\\d]".toRegex(), "").toInt()
+                        if (i1 > i2) {
+                            info("MythicMobs - Drop Random Error: " + mm.displayName + " - " + str)
                         } else {
-                            amount = args[1].replace("[^\\d]".toRegex(), "").toInt()
+                            amount = random().nextInt(i2 - i1 + 1) + i1
                         }
+                    } else {
+                        amount = args[1].replace("[^\\d]".toRegex(), "").toInt()
                     }
-                    repeat((0 until amount).count()) {
-                        ItemPlant.build(if (dropLockKiller) event.killer else null, args[0])?.let {
-                            drops.add(it)
-                        } ?: info("MythicMobs - Drop No Item: " + mm.displayName + " - " + args[0])
-                    }
+                }
+                repeat((0 until amount).count()) {
+                    ItemPlant.build(if (dropLockKiller) event.killer else null, args[0])?.let {
+                        drops.add(it)
+                    } ?: info("MythicMobs - Drop No Item: " + mm.displayName + " - " + args[0])
                 }
             }
         }
